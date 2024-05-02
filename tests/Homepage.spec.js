7f7a71d65ec1f63eb83849ef044e8e2d0bbab698
Funke <idowufunke32@gmail.com>
2024-05-02 13:34:26 +0100

    Second commit

--- tests/Homepage.spec.js
+++ tests/Homepage.spec.js
@@ -1,4 +1,7 @@
 import{test,expect} from '@playwright/test'
 test('Home page', async({page})=>{
     await page.goto('https://google.com');
+
+    expect(await page.goto('https://google.com')).toHaveText('google');
+    expect().toHaveLength();
 })
